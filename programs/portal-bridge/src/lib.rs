3f32927e941492b29e47616cf5a16a64fedde4c9
prtldotfun <prtldotfun@users.noreply.github.com>
2025-10-08 15:57:39 +0900

    feat: scaffold anchor program entry point

---
+++ programs/portal-bridge/src/lib.rs
@@ -0,0 +1,12 @@
+use anchor_lang::prelude::*;
+
+pub mod constants;
+pub mod error;
+pub mod state;
+
+declare_id!("PRT1xBridge1111111111111111111111111111111");
+
+#[program]
+pub mod portal_bridge {
+    use super::*;
+}
