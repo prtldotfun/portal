b1f5e72a70c1c409a28958f4f5c527cf596b4987
prtldotfun <prtldotfun@users.noreply.github.com>
2025-10-20 12:28:52 +0900

    feat: add fee calculation to BridgeConfig

--- programs/portal-bridge/src/state/bridge_config.rs
+++ programs/portal-bridge/src/state/bridge_config.rs
@@ -26,4 +26,55 @@ impl BridgeConfig {
     pub fn is_authority(&self, signer: &Pubkey) -> bool {
         self.authority == *signer
     }
+
+    pub fn is_relayer(&self, signer: &Pubkey) -> bool {
+        self.relayer == *signer
+    }
+
+    pub fn can_register_chain(&self) -> bool {
+        (self.registered_chains as usize) < MAX_CHAINS && !self.paused
+    }
+
+    pub fn calculate_fee(&self, amount: u64) -> Option<u64> {
+        amount
+            .checked_mul(self.fee_bps as u64)?
+            .checked_div(10_000)
+    }
+
+    pub fn increment_wrapped(&mut self, amount: u64) -> Result<()> {
+        self.total_wrapped = self
+            .total_wrapped
+            .checked_add(amount)
+            .ok_or(error!(crate::error::PortalError::ArithmeticOverflow))?;
+        Ok(())
+    }
+
+    pub fn increment_unwrapped(&mut self, amount: u64) -> Result<()> {
+        self.total_unwrapped = self
+            .total_unwrapped
+            .checked_add(amount)
+            .ok_or(error!(crate::error::PortalError::ArithmeticOverflow))?;
+        Ok(())
+    }
+
+    pub fn increment_intents(&mut self) -> Result<u64> {
+        let intent_id = self.total_intents;
+        self.total_intents = self
+            .total_intents
+            .checked_add(1)
+            .ok_or(error!(crate::error::PortalError::ArithmeticOverflow))?;
+        self.active_intents = self
+            .active_intents
+            .checked_add(1)
+            .ok_or(error!(crate::error::PortalError::ArithmeticOverflow))?;
+        Ok(intent_id)
+    }
+
+    pub fn decrement_active_intents(&mut self) -> Result<()> {
+        self.active_intents = self
+            .active_intents
+            .checked_sub(1)
+            .ok_or(error!(crate::error::PortalError::ArithmeticOverflow))?;
+        Ok(())
+    }
 }
