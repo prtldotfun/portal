d18728dd2a4d5c23ec86fa5a5ef525b32223daa9
prtldotfun <prtldotfun@users.noreply.github.com>
2026-01-07 01:37:29 +0900

    feat: add registerChain and createWrapper instruction builders

--- sdk/src/instructions/index.ts
+++ sdk/src/instructions/index.ts
@@ -118,3 +118,83 @@ export class PortalInstructions {
         authority: PublicKey,
         config: CreateWrapperConfig
     ): TransactionInstruction {
+        const [bridgeConfig] = deriveBridgeConfigPda(this.programId);
+        const [chainRegistry] = deriveChainRegistryPda(this.programId);
+        const [wrapperMint] = deriveWrapperMintPda(
+            config.sourceChainId,
+            config.sourceTokenAddress,
+            this.programId
+        );
+        const [wrapperMeta] = deriveWrapperMetaPda(wrapperMint, this.programId);
+
+        const nameBytes = Buffer.from(config.name, "utf-8");
+        const symbolBytes = Buffer.from(config.symbol, "utf-8");
+        const uriBytes = Buffer.from(config.metadataUri, "utf-8");
+
+        const bufferSize =
+            8 + 2 + 32 + 4 + symbolBytes.length + 4 + nameBytes.length + 1 + 1 + 4 + uriBytes.length;
+        const data = Buffer.alloc(bufferSize);
+        let offset = 0;
+
+        const disc = encodeInstructionDiscriminator("create_wrapper");
+        disc.copy(data, offset);
+        offset += 8;
+
+        data.writeUInt16LE(config.sourceChainId, offset);
+        offset += 2;
+        Buffer.from(config.sourceTokenAddress).copy(data, offset);
+        offset += 32;
+        data.writeUInt32LE(symbolBytes.length, offset);
+        offset += 4;
+        symbolBytes.copy(data, offset);
+        offset += symbolBytes.length;
+        data.writeUInt32LE(nameBytes.length, offset);
+        offset += 4;
+        nameBytes.copy(data, offset);
+        offset += nameBytes.length;
+        data.writeUInt8(config.decimals, offset);
+        offset += 1;
+        data.writeUInt8(config.sourceDecimals, offset);
+        offset += 1;
+        data.writeUInt32LE(uriBytes.length, offset);
+        offset += 4;
+        uriBytes.copy(data, offset);
+
+        return new TransactionInstruction({
+            keys: [
+                { pubkey: authority, isSigner: true, isWritable: true },
+                { pubkey: bridgeConfig, isSigner: false, isWritable: true },
+                { pubkey: chainRegistry, isSigner: false, isWritable: false },
+                { pubkey: wrapperMint, isSigner: false, isWritable: true },
+                { pubkey: wrapperMeta, isSigner: false, isWritable: true },
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+                { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },
+                { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
+            ],
+            programId: this.programId,
+            data,
+        });
+    }
+
+    wrapTokens(
+        relayer: PublicKey,
+        agentOwner: PublicKey,
+        config: WrapConfig
+    ): TransactionInstruction {
+        const [bridgeConfig] = deriveBridgeConfigPda(this.programId);
+        const [chainRegistry] = deriveChainRegistryPda(this.programId);
+        const [wrapperMeta] = deriveWrapperMetaPda(
+            config.wrapperMint,
+            this.programId
+        );
+        const [agentProfile] = deriveAgentPda(agentOwner, this.programId);
+
+        const data = Buffer.concat([
+            encodeInstructionDiscriminator("wrap_tokens"),
+            serializeWrapParams({
+                amount: config.amount,
+                sourceTxHash: config.sourceTxHash,
+            }),
+        ]);
+
+        return new TransactionInstruction({
